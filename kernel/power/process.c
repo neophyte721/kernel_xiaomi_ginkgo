a6ab38c2da387bd09b83ebf00b611fe6ff048c18
Yaroslav Furman <yaro330@gmail.com>
2019-12-06 03:07:04 +0300

    PM / sleep: Skip OOM killer toggles when kernel is compiled for Android
    Android devices use LMK algorythms, so there's no
    reason to disable and enable the OOM killer when entering and exiting
    suspend.
    
    This is a fixed version of https://github.com/YaroST12/VIOLENT_kernel/commit/86e59a93b2ef
    
    Co-authored-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Yaroslav Furman <yaro330@gmail.com>
    Signed-off-by: alk3pInjection <webmaster@raspii.tech>

--- kernel/power/process.c
+++ kernel/power/process.c
@@ -158,7 +158,7 @@ int freeze_processes(void)
 	pr_debug("\n");
 	BUG_ON(in_atomic());
 
-#ifndef CONFIG_HAVE_LOW_MEMORY_KILLER
+#ifndef CONFIG_ANDROID
 	/*
 	 * Now that the whole userspace is frozen we need to disbale
 	 * the OOM killer to disallow any further interference with
@@ -212,7 +212,7 @@ void thaw_processes(void)
 	pm_freezing = false;
 	pm_nosig_freezing = false;
 
-#ifndef CONFIG_HAVE_LOW_MEMORY_KILLER
+#ifndef CONFIG_ANDROID
 	oom_killer_enable();
 #endif
 
