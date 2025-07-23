f3ba47e2c637a94e5fb1813176cb2235a760ee7b
neophyte <neophyteplayground@gmail.com>
2025-07-23 11:40:30 +0700

    sched: cleanup!

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -2965,7 +2965,9 @@ extern int alloc_related_thread_groups(void);
 
 extern unsigned long all_cluster_ids[];
 
+#ifdef CONFIG_SCHED_WALT
 extern void check_for_migration(struct rq *rq, struct task_struct *p);
+#endif /* bye walt */
 
 static inline bool
 task_in_cum_window_demand(struct rq *rq, struct task_struct *p)
@@ -3051,7 +3053,9 @@ static inline bool task_placement_boost_enabled(struct task_struct *p)
 	return false;
 }
 
+#ifdef CONFIG_SCHED_WALT
 static inline void check_for_migration(struct rq *rq, struct task_struct *p) { }
+#endif /* bye walt */
 
 static inline int sched_boost(void)
 {
